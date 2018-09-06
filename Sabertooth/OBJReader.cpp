1bf341efc1b6980883dcbe085981d833799191fb
Pablo sK <pablo.190777@gmail.com>
2018-09-05 21:13:41 -0300

    Remoção dos ponteiros das classes

--- Sabertooth/OBJReader.cpp
+++ Sabertooth/OBJReader.cpp
@@ -1,5 +1,7 @@
 #include "OBJReader.h"
-
+#include "Mesh.h"
+#include <fstream>
+#include <sstream>
 
 
 OBJReader::OBJReader()
@@ -9,4 +11,37 @@ OBJReader::OBJReader()
 
 OBJReader::~OBJReader()
 {
+}
+
+Mesh *read(string filename) {
+	Mesh *mesh = new Mesh;
+	ifstream arq(filename);
+
+	while (!arq.eof()) {
+		string line;
+		getline(arq, line);
+		stringstream sline;
+		sline << line; string temp; sline >> temp;
+		if (temp == "v") {
+			// ler vértice ... 
+			float x, y, z;
+			sline >> x >> y >> z;
+			// ... atribuir vértices da malha // 
+		}
+		else if (temp == "f") {
+			// implementar lógica de varições 
+			// para face: v, v/t/n, v/t e v//n 
+			string token;
+			sline >> token;
+			// v/t/n, por exemplo 
+			stringstream stoken;
+			string aux;
+			getline(stoken, aux, '/'); //
+		}
+		else {
+			// else-if 
+		    // Verificar outras possibilidades: // g, vn, } 
+			return mesh;
+		}
+	}
 }
